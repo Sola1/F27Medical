4e392fda84e039d9c4fc7925ac8eb3cb7c54bb2e
ASUS-PC\ASUS <605113281@qq.com>
2017-05-08 14:50:32 +0800

    1

---
+++ F27Medical/app/src/main/java/com/liyan/asus/f27medical/MainActivity.java
@@ -0,0 +1,14 @@
+package com.liyan.asus.f27medical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //3333
+    }
+}
